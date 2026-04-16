2cf531370c0fce15ad5703615f24070c717b38ec
peetabourak-glitch <peetabourak@gmail.com>
2026-04-16 13:57:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = "nina-v1";
+const ASSETS = [
+  "/chat.html",
+  "/style.css",
+  "/script.js",
+  "/nina-hero.jpg",
+  "/icon-192.png",
+  "/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", e => {
+  // Netlify functions vždy ze sítě
+  if (e.request.url.includes("/.netlify/functions/")) {
+    return;
+  }
+
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      return cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      });
+    }).catch(() => caches.match("/chat.html"))
+  );
+});
